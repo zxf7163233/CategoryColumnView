a7a0c8015a6dd0b3910de8f4bd9cb7ad90d7356e
hysios hu <hysios@gmail.com>
2013-01-21 14:34:48 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CategoryColumnView::Application.config.secret_token = 'c992358c3f16d0147658d3c0b6c8ea77df99df494d8bcc54fce0a6b0b43b9580040027a320fbbc0b405a7956ab2f3ae03394949bf325b4922e864827caf80f1d'
